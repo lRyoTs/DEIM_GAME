db2bef1f54814d0cd45b4b1dab64c146bd0b3093
lRyoTs <114388830+lRyoTs@users.noreply.github.com>
2024-01-16 14:48:58 +0100

    movement scripts test

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -31,7 +31,7 @@ public class PlayerMovement : MonoBehaviour
         float verticalInput = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3 (horizontalInput,0f ,verticalInput).normalized;
         if (direction.magnitude >= 0.1f) {
-            float targetAngle = Mathf.Atan2(direction.x,direction.z) * Mathf.Rad2Deg;
+            float targetAngle = Mathf.Atan2(direction.x,direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f,angle,0f);
 
