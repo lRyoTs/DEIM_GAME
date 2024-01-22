9c7263a57308cbcb48ab7b359300cfec9941cd99
lRyoTs <114388830+lRyoTs@users.noreply.github.com>
2024-01-22 13:49:57 +0100

    Added Input System action
    Input system action for player movement and aim, need to create PlayerController

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,21 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 
-[RequireComponent(typeof(CharacterController))]
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
-    private CharacterController _characterController;
+    //private CharacterController _characterController;
     [SerializeField] private Transform cam;
 
-    public float speed = 6f;
+    public float speed = 3f;
     
     public float turnSmoothTime = 0.15f;
     private float turnSmoothVelocity;
 
     private void Awake()
     {
-        _characterController = GetComponent<CharacterController>();
+        //_characterController = GetComponent<CharacterController>();
     }
 
     private void Start()
@@ -29,15 +28,22 @@ public class PlayerMovement : MonoBehaviour
     {
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         float verticalInput = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3 (horizontalInput,0f ,verticalInput).normalized;
+        Vector3 direction = horizontalInput * cam.transform.right + verticalInput* cam.transform.forward;
+        
+        transform.Translate (direction.normalized * speed * Time.deltaTime,Space.World);
+
+       
+        /*
         if (direction.magnitude >= 0.1f) {
             float targetAngle = Mathf.Atan2(direction.x,direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f,angle,0f);
 
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            _characterController.Move(moveDir.normalized * speed * Time.deltaTime);
+            //Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            //_characterController.Move(moveDir.normalized * speed * Time.deltaTime);
         }
+        */
+        
     }
 
 }
